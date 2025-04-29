325511b948ce864f71770f01da101d130dc314bf
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-04-29 19:27:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ const header = document.getElementById("header")
 
 btnEmpezarPartida.addEventListener('click', () => {
 
+    const cronometro = document.getElementById("cronometro").value;
+    const contador = document.getElementById("contador").value;
+
     header.style.display = "none"; 
 
     table1.style.display = "none"; 
 
     tablero.style.display = "block"
 
+    tablero.innerHTML = `
+    <h2>Cronómetro${cronometro}</h2>
+    <h4>Intentos: ${contador}</h4>
+`;
 });
