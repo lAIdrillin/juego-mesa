4edec1edcae5115ca0ec17688c060acc488a68bd
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-04-29 18:28:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const btnEmpezarPartida = document.getElementById("empezar_partida");
 
+const tablero = document.getElementById("tablero")
+
+const main = document.getElementById("main")
+
+const header = document.getElementById("header")
+
 btnEmpezarPartida.addEventListener('click', () => {
     
+    tablero.style.display = "block";
+
+    header.style.display = "none"; 
+
+    main.style.display = "none"; 
     
 });
