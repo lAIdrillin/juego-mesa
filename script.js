de286b16a462b4eb64e01e97478dc0d7a79e0278
davidmata97-cell <daviddemata97@gmail.com>
2025-05-08 19:16:44 +0200

    Huevo de pascua

--- script.js
+++ script.js
@@ -207,6 +207,11 @@ function crearCartas(numero, tema) {
                 bloqueo = true;
 
                 if (primeraCarta.dataset.imagen === segundaCarta.dataset.imagen) {
+
+                    if (primeraCarta.dataset.imagen === "pollo.jpg") {
+                        mostrarMensaje("🐣 ¡Has desbloqueado el pollo de la suerte!");
+                    }
+                
                     primeraCarta.classList.add("bloqueada");
                     segundaCarta.classList.add("bloqueada");
                     verificarFinDeJuego();
@@ -233,7 +238,8 @@ function crearCartas(numero, tema) {
 
             if (cartasVolteadas.length === 2) {
                 contadorPares++; 
-                document.getElementById("contador").textContent = contadorPares; 
+                document.getElementById("contador").textContent = contadorPares;
+                
 
                 cartasVolteadas = []; 
             }
@@ -381,3 +387,15 @@ function mostrarHistorial() {
 btnLimpiarHistorial.addEventListener('click', () => {
     localStorage.clear();
 });
+
+function mostrarMensaje(texto) {
+    const mensaje = document.getElementById("mensajeJuego");
+    mensaje.textContent = texto;
+
+     
+       
+    
+    setTimeout(() => {
+        mensaje.textContent = "";
+    }, 3000);
+}
